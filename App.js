528e23fb10d45c44afd32b31e4d330dfcdfd3f37
Ivan Warburton <ivanmjwarburton@hotmail.co.uk>
2022-02-06 18:14:44 +0000

    First page build

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button, Alert, TextInput } from 'react-native';
+
+class welcomePage extends Component {
+    render() {
+        return (
+            <View style={[styles.container, { flexDirection: "column" }]}>
+
+                <View style={{ flex: 1 }}>
+
+                    <Text style={styles.mainTitle}>
+                        SpaceBook
+                    </Text>
+
+                    <Text style={styles.mainText}>
+                        Sign up or Sign in to your Spacebook acount.
+                    </Text>
+
+                </View>
+
+                <View style={{ flex: 2 }}>
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Username"
+                    />
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                    />
+
+                    <Button
+                        style={styles.button}
+                        title="Sign In"
+                        padding="10"
+                        onPress={() => Alert.alert('Signed in')}
+                    />
+                    <View style={styles.space} />
+                    <Button
+                        title="Sign Up"
+                        padding="10"
+                        onPress={() => Alert.alert('Signing Up')}
+                    />
+
+                </View>
+
+            </View>
 
 
-export default function App() {
-    return ( 
-    <View style = { styles.container } >
-        <Text > Open up App.js to start working on your app! </Text> <StatusBar style = "auto" />
-    </View>
-    );
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container:
+    {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        padding: 20
     },
+    mainTitle:
+    {
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 40
+    },
+    mainText:
+    {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    button:
+    {
+        padding: 10
+    },
+    input:
+    {
+        margin: 40,
+        padding: 10
+    },
+    space:
+    {
+        width: 20,
+        height: 20
+    }
 });
+
+export default welcomePage;
