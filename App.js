2a38eec67ab890f7b039f08e9f9bd88bbb8146c5
Ivan Warburton <ivanmjwarburton@hotmail.co.uk>
2022-02-08 12:04:52 +0000

    Navigation

--- App.js
+++ App.js
@@ -1,99 +1,26 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-class welcomePage extends Component {
-    render() {
+import SignInScreen from './components/signIn';
+import SignUpScreen from './components/signUp';
+
+
+const Drawer = createDrawerNavigator();
+
+class App extends Component{
+    render(){
         return (
-            <View style={styles.container}>
-
-                <View style={styles.container2}>
-                    <Image 
-                    style={styles.logo}
-                    source={require('./assets/Spacebook_Icon.png')} 
-                    />
-                    
-                    <Text style={styles.mainTitle}>
-                        SpaceBook
-                    </Text>
-                </View>
-
-                    <Text style={styles.mainText}>
-                        Sign up or Sign in to your Spacebook acount.
-                    </Text>
-
-                    <TextInput
-                            style={styles.input}
-                            placeholder="Username"
-                        />
-
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Password"
-                        />
-
-                        <Button
-                            style={styles.button}
-                            title="Sign In"
-                            padding="10"
-                        />
-                        
-                        <Button
-                            style={styles.button}
-                            title="Sign Up"
-                            padding="10"
-                        />
-
-            </View>
-
-
-        )
+            <NavigationContainer>
+                <Drawer.Navigator initialRouteName="Login">
+                    <Drawer.Screen name="Login" component={SignInScreen} />
+                    <Drawer.Screen name="Signup" component={SignUpScreen} />
+                </Drawer.Navigator>
+                
+            </NavigationContainer>
+        );
     }
 }
 
-const styles = StyleSheet.create({
-    container:
-    {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: 'space-evenly'
-    },
-    container2:
-    {
-        flexDirection: "row",
-        justifyContent: 'space-evenly'
-    },
-    mainTitle:
-    {
-        fontSize: 40,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    },
-    mainText:
-    {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    },
-    button:
-    {
-        padding: 10
-    },
-    input:
-    {
-        margin: 40,
-        padding: 10
-    },
-    space:
-    {
-        width: 20,
-        height: 20
-    },
-    logo:
-    {
-        width: 100,
-        height: 100,
-        resizeMode: 'contain'
-    }
-});
-
-export default welcomePage;
+export default App;
---
+++ components/signIn.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button, TextInput, Image } from 'react-native';
+
+class signIn extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <View style={styles.container2}>
+                    
+                    {/*<Image 
+                    style={styles.logo}
+                    source={require('./assets/Spacebook_Icon.png')} 
+                    />
+                    */}
+                    
+                    <Text style={styles.mainTitle}>
+                        SpaceBook
+                    </Text>
+                </View>
+
+                    <Text style={styles.mainText}>
+                        Sign up or Sign in to your Spacebook acount.
+                    </Text>
+
+                    <TextInput
+                            style={styles.input}
+                            placeholder="Email"
+                        />
+
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Password"
+                        />
+
+                        <Button
+                            style={styles.button}
+                            title="Sign In"
+                            padding="10"
+                        />
+                        
+                        <Button
+                            style={styles.button}
+                            title="Sign Up"
+                            padding="10"
+                        />
+
+            </View>
+
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:
+    {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: 'space-evenly'
+    },
+    container2:
+    {
+        flexDirection: "row",
+        justifyContent: 'space-evenly'
+    },
+    mainTitle:
+    {
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    mainText:
+    {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    button:
+    {
+        padding: 10
+    },
+    input:
+    {
+        margin: 40,
+        padding: 10
+    },
+    space:
+    {
+        width: 20,
+        height: 20
+    },
+    logo:
+    {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain'
+    }
+});
+
+export default signIn;
