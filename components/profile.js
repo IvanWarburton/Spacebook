41e35c0499bffd1628b9158d37cafd146ce3245e
Ivan Warburton <ivanmjwarburton@hotmail.co.uk>
2022-03-03 17:44:30 +0000

    All end points complete (bug in likes)

--- components/profile.js
+++ components/profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button, Image, Modal, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import TouchHistoryMath from 'react-native/Libraries/Interaction/TouchHistoryMath';
 
 class Profile extends Component {
 
@@ -24,6 +25,9 @@ class Profile extends Component {
             postModalVisible: false,
             modalIsLoading: false,
             noPostsFound: true,
+            isUsersPost: false,
+            alertModalVisible: false,
+            alertModalMesssage: "",
             post: { text: "" }
         }
 
@@ -354,19 +358,52 @@ class Profile extends Component {
                         {console.log("code loading before suposed too here")}
                         <Button title="Close" onPress={() => this.setState({ [modalName]: false })} />
                     </View>
-                    
                     )}
 
                     {!this.state.isLoadingAPost && (
                     <View style={styles.modalView}>
-                        <Text>
+                        
+                        {!this.state.isUsersPost && (
+                        <View>
+                            <Text>
                             {this.state.viewingPost.author.first_name} {this.state.viewingPost.author.last_name}: {this.state.viewingPost.text}
-                        </Text>
+                            </Text>
 
-                        <Text>
-                            Likes: {this.state.viewingPost.numLikes} Date: {this.state.viewingPost.timestamp.slice(0, 10)} Time: {this.state.viewingPost.timestamp.slice(12, 16)}
-                        </Text>
-                        <Button title="Close" onPress={() => this.resetModal(postID)} />
+                            <Text>
+                                Likes: {this.state.viewingPost.numLikes} Date: {this.state.viewingPost.timestamp.slice(0, 10)} Time: {this.state.viewingPost.timestamp.slice(12, 16)}
+                            </Text>
+                        </View>
+                        )}
+
+                        {this.state.isUsersPost && (
+                        <View>
+                            <Text>
+                            {this.state.viewingPost.author.first_name} {this.state.viewingPost.author.last_name}: 
+                            </Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder= {this.state.viewingPost.text}
+                                onChangeText={(post) => this.state.viewingPost.text = post}
+                            />
+                        </View>
+                        )}
+
+                        {this.state.isUsersPost && (
+                        <View style={styles.container2}>
+                            <Button title="Close" onPress={() => this.resetModal(postID)} />
+                            <Button title="Update Post" onPress={() => this.updatePost(postID)} />
+                            <Button title="Delete" onPress={() => this.deletePost(postID)} />
+                        </View>
+                        )}
+
+                        {!this.state.isUsersPost && (
+                        <View style={styles.container2}>
+                            <Button title="Like" onPress={() => this.likePost(postID)} />
+                            <Button title="UnLike" onPress={() => this.unLikePost(postID)} />
+                            <Button title="Close" onPress={() => this.resetModal(postID)} />
+                        </View>
+                        )}
+
                     </View>
                     )}
 
@@ -379,6 +416,7 @@ class Profile extends Component {
     resetModal(postID)
     {
         this.setState({["postModal" + postID + "Visible"]: false});
+        this.setState({isUsersPost: false});
     }
 
     async viewPost(postID) 
@@ -413,6 +451,8 @@ class Profile extends Component {
                     isLoadingAPost: false,
                     viewingPost: responseJson
                 })
+                if(this.state.viewingPost.author.user_id == this.state.loggedInUser)
+                {this.setState({isUsersPost: true})}
             })
             .catch((error) => {
                 console.log(error);
@@ -452,6 +492,146 @@ class Profile extends Component {
             .catch((error) => {
                 console.log(error);
             })
+    }
+
+    async updatePost(postID) {
+        let user = this.state.loggedInUser;
+   
+        this.setState({isLoadingPosts: true });
+
+        return fetch("http://localhost:3333/api/1.0.0/user/" + user + "/post/" + postID, {
+            method: 'PATCH',
+            headers:
+            {
+                'Content-Type': 'application/json',
+                'X-Authorization': this.state.sessionToken
+            },
+            body: JSON.stringify(this.state.viewingPost)
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("Updated");
+                    this.setState({["postModal" + postID + "Visible"]: false});
+                    this.getPosts();
+                } else if (response.status === 400) {
+                    console.log("Bad Request");
+                } else if (response.status === 401) {
+                    console.log("Unauthorise");
+                } else if (response.status === 403) {
+                    console.log("Forbidden - you can only update your own posts");
+                } else if (response.status === 404) {
+                    console.log("Not Found");
+                } else if (response.status === 500) {
+                    console.log("Server Error");
+                } else {
+                    throw 'Something went wrong';
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    async deletePost(postID) {
+        let user = this.state.loggedInUser;
+        this.setState({ isLoadingPosts: true });
+
+        return fetch("http://localhost:3333/api/1.0.0/user/" + user + "/post/" + postID, {
+            method: 'DELETE',
+            headers:{'X-Authorization': this.state.sessionToken}
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("Post Delete");
+                    this.setState({["postModal" + postID + "Visible"]: false});
+                    this.getPosts();
+                } else if (response.status === 401) {
+                    console.log("Unauthorised");
+                } else if (response.status === 403) {
+                    console.log("You can only delete your own posts");
+                } else if (response.status === 404) {
+                    console.log("Not found");
+                } else if (response.status === 500) {
+                    console.log("Server Error");
+                } else {
+                    throw 'Something went wrong';
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    async likePost(postID) {
+        let user = this.state.loggedInUser;
+        this.setState({ isLoadingPosts: true });
+
+        return fetch("http://localhost:3333/api/1.0.0/user/" + user + "/post/" + postID + "/like", {
+            method: 'POST',
+            headers:{'X-Authorization': this.state.sessionToken}
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("Post Liked");
+                    this.setState({["postModal" + postID + "Visible"]: false, isLoadingPosts: false});
+                    this.getPosts();
+                } else if (response.status === 401) {
+                    console.log("Unauthorised");
+                } else if (response.status === 403) {
+                    console.log("You have already liked the post");
+                    this.setState({["postModal" + postID + "Visible"]: false, isLoadingPosts: false});
+                    this.alertMessage("You have already liked the post.");
+                } else if (response.status === 404) {
+                    console.log("Not found");
+                } else if (response.status === 500) {
+                    console.log("Server Error");
+                } else {
+                    throw 'Something went wrong';
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    async unLikePost(postID) {
+        let user = this.state.loggedInUser;
+        this.setState({ isLoadingPosts: true });
+
+        return fetch("http://localhost:3333/api/1.0.0/user/" + user + "/post/" + postID + "/like", {
+            method: 'DELETE',
+            headers:{'X-Authorization': this.state.sessionToken}
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("Post UnLiked");
+                    this.setState({["postModal" + postID + "Visible"]: false, isLoadingPosts: false});
+                    this.getPosts();
+                } else if (response.status === 401) {
+                    console.log("Unauthorised");
+                } else if (response.status === 403) {
+                    console.log("You have not liked this post");
+                    this.setState({["postModal" + postID + "Visible"]: false, isLoadingPosts: false});
+                    this.alertMessage("You have not liked this post.");
+                } else if (response.status === 404) {
+                    console.log("Not found");
+                } else if (response.status === 500) {
+                    console.log("Server Error");
+                } else {
+                    throw 'Something went wrong';
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    alertMessage(message)
+    {
+        this.setState({
+            alertModalVisible: true,
+            alertModalMesssage: message
+        })
     }
 
     render() {
@@ -537,6 +717,20 @@ class Profile extends Component {
                         <this.postLists />
                     </ScrollView>
 
+                    <View>
+                        <Modal visible={this.state.alertModalVisible}
+                            animationType="fade"
+                            transparent={true}>
+
+                            <View style={styles.alertModal}>
+                                <Text>ALERT</Text>
+                                <Text>{this.state.alertModalMesssage}</Text>
+                                <Button title="Close" onPress={() => this.setState({ alertModalVisible: false })} />
+                            </View>
+
+                        </Modal>
+                    </View>
+
                 </View>
             )
         }
@@ -591,12 +785,36 @@ const styles = StyleSheet.create({
         height: 200,
         borderRadius: 200/2
     },
+    input:
+    {
+        margin: 40,
+        padding: 10,
+        width: '70%'
+    },
     modalView:
     {
         flex: 1,
         margin: 20,
-        marginVertical: "50%",
+        marginVertical: "80%",
         backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 1
+    },
+    alertModal:
+    {
+        flex: 1,
+        margin: 20,
+        marginVertical: "90%",
+        backgroundColor: "#ffd4d8",
         borderRadius: 20,
         padding: 35,
         alignItems: "center",
